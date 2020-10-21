70670f088710bc72909cceab4aeef0edc141a8f3
mauro-nieva <mauro_ramones_86@hotmail.com>
2020-10-21 19:52:24 -0300

    Commit inicial

---
+++ CODIGO/app/src/main/java/com/example/prueba4/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.prueba4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "HOLA MUNDO", Toast.LENGTH_LONG).show();
+    }
+}
